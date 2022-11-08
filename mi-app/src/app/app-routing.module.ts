f4e9c2d36d6fd1ec69b02f71bf7c75028d1b4c2b
vitacell2 <vita6cell@gmail.com>
2022-11-08 13:44:15 +0100

    08-11-2022(2)

--- mi-app/src/app/app-routing.module.ts
+++ mi-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarPedidoComponent } from './lista-pedidos/editar-pedido/editar-pedido.component';
+import { ListaPedidosComponent } from './lista-pedidos/lista-pedidos.component';
+import { PedidoComponent } from './lista-pedidos/pedido/pedido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: ListaPedidosComponent},
+{path: 'pedido', component: PedidoComponent},
+{path: 'pedido/edit', component: EditarPedidoComponent},
+{path: 'pedido/:id', component: PedidoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mi-app/src/app/lista-pedidos/pedido/pedido.component.ts
+++ mi-app/src/app/lista-pedidos/pedido/pedido.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pedido } from 'src/app/model/pedido';
+import { LogService } from 'src/app/servicios/log.service';
 
 @Component({
   selector: 'app-pedido',
@@ -10,16 +11,20 @@ export class PedidoComponent implements OnInit {
 
   pedido: Pedido;
 
-  constructor()
+  constructor(private log: LogService, private route: ActivatedRoute)
   {
-    this.pedido =
+   /* this.pedido =
     {
       id:1,
       desc:'Zapatos',
       entregado: false,
       fechaPedido: new Date(),
       user: 'Luis'
-    };
+    };*/
+
+    // route es un objeto que tiene la URL actual pedido/:id
+    this.pedido.id = this.route.snapshot.params['id'];
+
   }
 
   ngOnInit(): void {
